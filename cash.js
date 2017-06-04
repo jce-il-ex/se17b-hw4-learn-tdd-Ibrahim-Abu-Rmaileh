40b9dc63dbb848a71aafab5c548ece26ddc6c16a
Ibrahim-Abu-Rmaileh <ibrahim.abu.rmaileh@gmail.com>
2017-06-04 13:55:05 +0100

    GREEN: Test done successfully!

--- cash.js
+++ cash.js
@@ -1,1 +1,6 @@
-//nothing to write yet 
+var C = {};                    // C Object simplifies exporting the module
+C.getChange = function () {    // enough to satisfy the test
+    'use strict';
+    return true;               // also passes JSLint
+};
+module.exports = C;            // export the module with a single method
